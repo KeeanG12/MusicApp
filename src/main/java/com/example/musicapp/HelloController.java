1a7fc069b5b54258a642ef5d0ea76fd394c87f4c
keg21 <keg21@aber.ac.uk>
2022-02-14 10:46:43 +0000

    Got Music to play on button click

--- src/main/java/com/example/musicapp/HelloController.java
+++ src/main/java/com/example/musicapp/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
     protected void onHelloButtonClick() throws URISyntaxException {
         String path = "Stronger.mp3";
         
-        Media sound = new Media(getClass().getResource("/Stronger.mp3").toURI().toString());
+        Media sound = new Media(getClass().getResource("/Free Lunch.mp3").toURI().toString());
         MediaPlayer player = new MediaPlayer(sound);
         player.play();
     }
